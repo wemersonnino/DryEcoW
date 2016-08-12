4918890c1063cfcb8df2a6989ed138f24ecfb8be
wemerson <wemersonnino@gmail.com>
2016-08-12 16:22:18 -0300

    criação das tarefas

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+// Aqui nós carregamos o gulp e os plugins através da função `require` do nodejs
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+// Definimos o diretorio dos arquivos para evitar repetição futuramente
+var files = "./src/*.js";
+
+//Aqui criamos uma nova tarefa através do ´gulp.task´ e damos a ela o nome 'lint'
 
 gulp.task('default', function () {
     // place code for your default task here
+    // Aqui carregamos os arquivos que a gente quer rodar as tarefas com o `gulp.src`
+    // E logo depois usamos o `pipe` para rodar a tarefa `jshint`
+    gulp.src(files)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+//Criamos uma tarefa 'default' que vai rodar quando rodamos `gulp` no projeto
+gulp.task('default', function() {
+ 
+// Usamos o `gulp.run` para rodar as tarefas
+// E usamos o `gulp.watch` para o Gulp esperar mudanças nos arquivos para rodar novamente
+gulp.run('lint', 'dist');
+gulp.watch(files, function(evt) {
+gulp.run('lint', 'dist');
+});
 });
